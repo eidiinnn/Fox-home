55bbf8981a7f1368d04a336a96b9a844e38b0181
Eduardo da Silva <67817853+eidiinnn@users.noreply.github.com>
2022-02-03 17:09:40 -0300

    add the background

--- src/style.js
+++ src/style.js
@@ -1,8 +1,16 @@
 import styled from "styled-components";
 import { createGlobalStyle } from "styled-components";
 
+import backgroundImageBlur from "./images/backgroundImageBlur.jpg";
+
 export const GlobalStyle = createGlobalStyle` 
 body {
   margin: 0;
-  padding: 0;
+  padding: 10px;
+  width: 100%;
+  height: 100%;
+  background: url(${backgroundImageBlur}) no-repeat;
+  background-position: center;
+  background-size: cover;
+  background-attachment: fixed;
 }`;
