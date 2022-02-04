665ab9c30bf0f0ad8425a88ac80c3b26bf743c14
Eduardo da Silva <67817853+eidiinnn@users.noreply.github.com>
2022-02-04 13:48:15 -0300

    Create bookmarkLinks.js

---
+++ src/bookmarkLinks.js
@@ -0,0 +1,7 @@
+import { FaDiscord, FaGithub, FaReddit } from "react-icons/fa";
+
+export const bookmarkLinks = [
+  ["https://discord.com/channels/@me", FaDiscord],
+  ["https://github.com/", FaGithub],
+  ["https://www.reddit.com/", FaReddit],
+];
