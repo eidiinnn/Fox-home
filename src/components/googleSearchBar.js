3238d467dec97fcb375531a743406d386e69f162
Eduardo da Silva <67817853+eidiinnn@users.noreply.github.com>
2022-02-05 03:39:38 -0300

    make search works

--- src/components/googleSearchBar.js
+++ src/components/googleSearchBar.js
@@ -2,15 +2,28 @@ import React from "react";
 import { Component } from "react";
 import { FaGoogle } from "react-icons/fa";
 
-import { SearchBarContainer, SearchBarGoogleIcon } from "../style";
+import {
+  SearchBarContainer,
+  SearchBarGoogleIcon,
+  SearchBarInput,
+  SearchBarForm,
+} from "../style";
 
 export default class GoogleSearchBar extends Component {
+  submitEvent = (event) => {
+    event.preventDefault();
+    window.location.href = `https://www.google.com/search?q=${event.target.search.value}`;
+  };
+
   render() {
     return (
       <SearchBarContainer>
         <SearchBarGoogleIcon>
           <FaGoogle />
         </SearchBarGoogleIcon>
+        <SearchBarForm onSubmit={this.submitEvent}>
+          <SearchBarInput id="search" placeholder="Search on google" />
+        </SearchBarForm>
       </SearchBarContainer>
     );
   }
