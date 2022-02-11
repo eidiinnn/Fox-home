93f3c446f87b99f871e8bfb08bfb4fc6171ef928
Eduardo da Silva <67817853+eidiinnn@users.noreply.github.com>
2022-02-11 03:25:20 -0300

    add default settings

---
+++ src/defaultSettings.js
@@ -0,0 +1,6 @@
+import image from "../../images/image.jpg";
+
+export default function defaultSettings() {
+  if (!localStorage.getItem("AmPm")) localStorage.setItem("AmPm", false);
+  if (!localStorage.getItem("image")) localStorage.setItem("image", image);
+}
