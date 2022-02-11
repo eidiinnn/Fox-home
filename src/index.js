e7d91235c7c049cb20c9cee036e43c3d2ba52cb3
Eduardo da Silva <67817853+eidiinnn@users.noreply.github.com>
2022-02-11 03:27:15 -0300

    call default settings

--- src/index.js
+++ src/index.js
@@ -3,9 +3,12 @@ import ReactDOM from "react-dom";
 
 import CentralDiv from "./components/centralDiv";
 import GoogleSearchBar from "./components/googleSearchBar";
-import Settings from "./components/tools/settings";
+import Settings from "./components/settings";
+import defaultSettings from "./defaultSettings";
 
 import { GlobalStyle, MainGrind, Background } from "./style";
+
+defaultSettings();
 
 ReactDOM.render(
   <React.StrictMode>
