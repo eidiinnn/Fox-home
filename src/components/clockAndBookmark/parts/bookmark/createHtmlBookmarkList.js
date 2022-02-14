58b8012075e0ea81b7b8ab98d570bc8ecc311515
Eduardo da Silva <67817853+eidiinnn@users.noreply.github.com>
2022-02-14 19:47:11 -0300

    createHtmlBookmarkList use redux

--- src/components/clockAndBookmark/parts/bookmark/createHtmlBookmarkList.js
+++ src/components/clockAndBookmark/parts/bookmark/createHtmlBookmarkList.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Component } from "react";
+import { useSelector } from "react-redux";
 import { iconsList } from "./tools/iconsList.js";
 import extractDomain from "extract-domain";
 
@@ -9,54 +9,35 @@ import {
   NotListedIcon,
 } from "../../../../style";
 
-export default class CreateHtmlBookmarkList extends Component {
-  componentDidMount = () => {
-    const storageBookmark = localStorage.getItem("bookmark");
-    !storageBookmark
-      ? localStorage.setItem(
-          "bookmark",
-          JSON.stringify(this.state.bookmarkLinks)
-        )
-      : this.setState({ bookmarkLinks: JSON.parse(storageBookmark) });
-  };
+export default function CreateHtmlBookmarkList() {
+  const bookmarkLinks = useSelector((state) => state.bookmarkLinks);
 
-  filterUrl = (url) => extractDomain(url, { tld: true });
+  function filterUrl(url) {
+    return extractDomain(url, { tld: true });
+  }
 
-  getAListedIcon = (url) => {
-    const domain = this.filterUrl(url);
+  function getAListedIcon(url) {
+    const domain = filterUrl(url);
     const iconsListReturn = iconsList.find((item) => item[0] === domain);
     return !iconsListReturn ? null : iconsListReturn[1];
-  };
-
-  getAIcon(url) {
-    const Icon = this.getAListedIcon(url);
-    const FirstLetter = this.filterUrl(url).slice(0, 1).toUpperCase();
-    return !Icon ? <NotListedIcon>{FirstLetter}</NotListedIcon> : <Icon />;
   }
 
-  state = {
-    bookmarkLinks: [
-      "https://discord.com/channels/@me",
-      "https://github.com/",
-      "https://www.reddit.com/",
-      "https://www.linkedin.com/",
-      "https://web.whatsapp.com/",
-      "https://www.notion.so/",
-    ],
-  };
-
-  render() {
-    return (
-      <CentralDivBookmarkUl>
-        {this.state.bookmarkLinks.map((url) => {
-          if (!this.filterUrl(url)) return null;
-          return (
-            <CentralDivBookmarkLi key={url}>
-              <a href={url}>{this.getAIcon(url)}</a>
-            </CentralDivBookmarkLi>
-          );
-        })}
-      </CentralDivBookmarkUl>
-    );
+  function getAIcon(url) {
+    const Icon = getAListedIcon(url);
+    const firstLetter = filterUrl(url).slice(0, 1).toUpperCase();
+    return !Icon ? <NotListedIcon>{firstLetter}</NotListedIcon> : <Icon />;
   }
+
+  return (
+    <CentralDivBookmarkUl>
+      {bookmarkLinks.map((url) => {
+        if (!filterUrl(url)) return null;
+        return (
+          <CentralDivBookmarkLi key={url}>
+            <a href={url}>{getAIcon(url)}</a>
+          </CentralDivBookmarkLi>
+        );
+      })}
+    </CentralDivBookmarkUl>
+  );
 }
