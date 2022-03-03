5c4243b592b8933feac0dfd22c8ff3673aa78be4
Eduardo da Silva <67817853+eidiinnn@users.noreply.github.com>
2022-03-03 13:51:33 -0300

    code changes

--- src/components/settings/options/backgroundImage.js
+++ src/components/settings/options/backgroundImage.js
@@ -1,35 +1,32 @@
 import React from "react";
-import { useDispatch } from "react-redux";
 import Resizer from "react-image-file-resizer";
-
+import { useDispatch } from "react-redux";
 import { SettingsModalItems, SettingsModalTitles } from "../../../style";
+
+function resizeFile(file) {
+  return new Promise((resolve) => {
+    Resizer.imageFileResizer(
+      file,
+      1280,
+      720,
+      "PNG",
+      50,
+      0,
+      (uri) => {
+        resolve(uri);
+      },
+      "blob"
+    );
+  });
+}
 
 export default function BackgroundImage() {
   const dispatch = useDispatch();
 
-  const resizeFile = (file) =>
-    new Promise((resolve) => {
-      Resizer.imageFileResizer(
-        file,
-        1280,
-        720,
-        "PNG",
-        50,
-        0,
-        (uri) => {
-          resolve(uri);
-        },
-        "blob"
-      );
-    });
-
   async function imageUploadOnDone(event) {
     const blobImage = new Blob(event.target.files, { type: "image/png" });
     const resizedImage = await resizeFile(blobImage);
-    dispatch({
-      type: "IMAGE_CHANGE",
-      image: resizedImage,
-    });
+    dispatch({ type: "IMAGE_CHANGE", image: resizedImage });
   }
 
   return (
