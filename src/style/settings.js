dfbeec7f5679f88e903c1020a5791c806bd2a4d4
Eduardo da Silva <eduardosilva.pn@gmail.com>
2022-05-12 14:01:27 -0300

    bug fix

--- src/style/settings.js
+++ src/style/settings.js
@@ -14,7 +14,7 @@ export const OpenSettingsModalIcon = styled.div`
   margin-top: 1.25rem;
   margin-left: 1.25rem;
 
-  color: ${(props) => props.color};
+  color: ${(props) => (!props.color ? normalTextColor : props.color)};
   font-size: 1.35rem;
 
   transition: 500ms;
